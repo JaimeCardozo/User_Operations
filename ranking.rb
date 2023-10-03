0416f40b7b4ad1b07b497988a69bf1e6a3cfca75
Jaime Cardozo <jaime.cardozo172@gmail.com>
2023-10-03 13:02:13 -0500

    ranking

--- ranking.rb
+++ ranking.rb
@@ -2,11 +2,8 @@ path = '\Users\kike\Desktop\Programación\User_Operations\records'
 ranking = {}
 Dir.foreach(path) do |filename|
        next if filename == '.' || filename == '..'
-      
-       puts "working on #{filename}"
 
        name = filename.gsub(".txt","")
-       puts name 
        
        file = File.open("#{path}/#{filename}", 'r')
      
@@ -17,3 +14,13 @@ Dir.foreach(path) do |filename|
      
        # more code
      end
+
+puts ranking
+ranking = ranking.sort_by(&:last)
+ranking = ranking.reverse
+i = 1
+ranking.each do |date|
+  #puts "#{date[1]}. #{date[0]}"
+  puts "#{i}. #{date[0]} -------- #{date[1]}"
+  i += 1
+end
